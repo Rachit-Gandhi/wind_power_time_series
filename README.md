11c6e8f15021bba4f45bcd6e23a203a8d70fdfa2
Gandhi <rachitgandhi2002@gmail.com>
2023-07-18 20:33:05 +0530

    main my code

---
+++ README.md
@@ -0,0 +1,2 @@
+# wind_power_time_series
+Project under summer internship at KPMG 2023 under Management Advisory at Industry 4.0 Team
---
+++ file_feed.py
@@ -0,0 +1,19 @@
+import y_data_preprocess as y_data
+import os
+
+directory = input('Please enter the folder:')  # Replace with the actual directory path
+year_repo = input('Please enter the year:')
+# List all files in the directory
+files = os.listdir(directory)
+
+# Filter CSV files with names starting with 'Kelmarsh'
+csv_files = [file for file in files if file.endswith('.csv') and file.startswith('Turbine')]
+
+# Print the report of all listed CSV files
+for file in csv_files:
+    file_path = os.path.join(directory, file)
+    report_nam = f"Report_{file[:24]}"
+    report_name = f"{report_nam}_{year_repo}"
+    y_data.y_data_analyse(file_path, report_name)
+    print("Report Published")
+
